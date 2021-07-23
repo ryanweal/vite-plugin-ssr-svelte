ccf5ab1f4c807b845f3fede5dacb747bea62f8a7
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-07-23 17:59:23 +0100

    Update title and description in page

--- pages/_default/_default.page.server.js
+++ pages/_default/_default.page.server.js
@@ -13,8 +13,8 @@ async function render(pageContext) {
 
   // See https://vite-plugin-ssr.com/html-head
   const { documentProps } = pageContext
-  const title = (documentProps && documentProps.title) || 'Vite SSR app'
-  const desc = (documentProps && documentProps.description) || 'App using Vite + vite-plugin-ssr'
+  const title = (documentProps && documentProps.title) || 'Vite SSR app (Svelte version)'
+  const desc = (documentProps && documentProps.description) || 'App using Vite + vite-plugin-ssr (Svelte version)'
 
   return html`<!DOCTYPE html>
     <html lang="en">
