9016abcc7943797cb67ec3eae04b32980f3f46c9
Ryan Weal <ryan@weal.ca>
2022-10-17 17:44:28 -0400

    Create assets folder for logo

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,8 @@ import ssr from 'vite-plugin-ssr/plugin'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default {
-  //base: '/some/base/url',
+  publicDir: "assets",
   plugins: [svelte(), ssr({
-    prerender: {
-      //partial: true,
-      //noExtraDir: true,
-      //parallel: 1 // Can be `number` or `boolean`
-    }
+    prerender: true
   })]
 }
