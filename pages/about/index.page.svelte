01dd98cdcf08c0434ca484b58bb710569794d60f
Ryan Weal <ryan@weal.ca>
2022-10-22 11:03:29 -0400

    CSS scope example

--- pages/about/index.page.svelte
+++ pages/about/index.page.svelte
@@ -6,17 +6,16 @@
 
 <MetaTags title={pageProps.title} description={pageProps.body} />
 
-<main>
-  <Layout>
-    <h1>{pageProps.title}</h1>
-    <p>{pageProps.body}</p>
-  </Layout>
-</main>
+<Layout>
+  <h1>{pageProps.title}</h1>
+  <p>{pageProps.body}</p>
+</Layout>
 
-<style>
-  /* Note: for * :global() to work, the component must be within
-    ===== a parent DOM element (in this case, the <main> block). */
-  * :global(h1), * :global(p) {
-    color: green;
-  }
+<style scoped>
+h1 {
+  color: darkgreen;
+}
+p {
+  color: green;
+}
 </style>
