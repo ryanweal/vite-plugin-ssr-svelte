dfc25c4293c15cc66d527c0f1c5f4e25744c4527
Ryan Weal <ryan@weal.ca>
2022-10-22 10:36:09 -0400

    Ensure clean build directory each time

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import ssr from 'vite-plugin-ssr/plugin'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 export default {
+  emptyOutDir: true,
   publicDir: "assets",
   plugins: [svelte({
     compilerOptions: {
