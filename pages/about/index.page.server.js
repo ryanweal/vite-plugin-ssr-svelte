5aafae07a00b7c65b9e8d2bbdc0fe72928e9028f
Ryan Weal <ryan@weal.ca>
2022-10-27 23:59:05 -0400

    Update text

--- pages/about/index.page.server.js
+++ pages/about/index.page.server.js
@@ -6,7 +6,7 @@ async function onBeforeRender(pageContext) {
   // await api data here
   const page = {
     title: "About",
-    body: "Sample API prefetch page"
+    body: "Sample API prefetch page with a fixed path (ie, not a catch-all route)."
   };
 
   // pageProps is listed in server's passToClient list, so we put all of our
